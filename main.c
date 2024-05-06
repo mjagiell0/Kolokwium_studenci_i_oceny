d95adfbc1812d7fad6cb83a77bfa32843ba7992e
mjagiell0 <m.jagiello03@gmail.com>
2024-05-06 23:56:18 +0200

    Przykładowe ciało funkcji main

--- main.c
+++ main.c
@@ -3,7 +3,19 @@
 
 int main(void) {
     struct student_t **students = NULL;
-    read_students_grades("test.txt", &students);
+    int result = read_students_grades("students_grades.txt", &students);
+
+    if(result == -2){
+        printf("Couldn't open file");
+        return 4;
+    }else if(result == -3){
+        printf("File corrupted");
+        return 6;
+    }else if(result){
+        printf("Failed to allocate memory");
+        return 8;
+    }
+
     display_students_grades(students);
     free_students_grades(students);
     return 0;
